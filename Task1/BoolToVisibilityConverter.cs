c0c6016c54c5db30e2c151e247786b36415f6f61
vrsshk <146523898+vrsshk@users.noreply.github.com>
2024-11-14 21:42:39 +0300

    Add files via upload

---
+++ Task1/BoolToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace GeometryApp.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool && (bool)value;  // true - элемент виден, false - скрыт
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool && (bool)value;
+        }
+    }
+}
