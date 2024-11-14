90ab083514cdb70ab87a09b8c1dc6e44053cd033
vrsshk <146523898+vrsshk@users.noreply.github.com>
2024-11-14 21:41:20 +0300

    Add files via upload

---
+++ Task1/ShapeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GeometryApp.Models
+{
+    public class ShapeModel
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Diameter { get; set; }
+    }
+}
