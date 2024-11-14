72697a9dcdfecb6488c786f64ac0426fb2e94574
vrsshk <146523898+vrsshk@users.noreply.github.com>
2024-11-14 21:43:29 +0300

    Add files via upload

---
+++ Task1/Converters/HalfHeightConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace GeometryApp
+{
+    public class HalfHeightConverter : IMultiValueConverter
+    {
+        public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (values[0] is double height)
+            {
+                return (400 - height) / 2; // Используйте высоту Canvas
+            }
+            return 0;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Task1/Converters/HalfWidthConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace GeometryApp
+{
+    public class HalfWidthConverter : IMultiValueConverter
+    {
+        public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (values[0] is double width)
+            {
+                return (600 - width) / 2; // Используйте ширину Canvas
+            }
+            return 0;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
