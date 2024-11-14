56b4bd77b58b49e2c6e470f031c60335765bc4c7
vrsshk <146523898+vrsshk@users.noreply.github.com>
2024-11-14 21:44:01 +0300

    Add files via upload

---
+++ Task1/RelayCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace GeometryApp.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+
+        public RelayCommand(Action execute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+}
